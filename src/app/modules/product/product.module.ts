dbc3f9296bd7218e7702242dffc9d4c17d53451d
Firthuns2021 <86574425+Firthuns2021@users.noreply.github.com>
2022-08-05 19:07:09 +0200

    Frontend segunda parte: CRUD product.

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { NewProductComponent } from './new-product/new-product.component';
 import {CommonModule} from "@angular/common";
 import {ProductComponent} from "./product/product.component";
+import {MaterialModule} from "../material/material.module";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 
@@ -12,7 +15,11 @@ import {ProductComponent} from "./product/product.component";
     NewProductComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductModule { }
--- src/app/modules/product/product/product.component.ts
+++ src/app/modules/product/product/product.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ConfirmComponent} from "../../shared/components/confirm/confirm.component";
+import {NewProductComponent} from "../new-product/new-product.component";
+import {MatSnackBar, MatSnackBarRef, SimpleSnackBar} from "@angular/material/snack-bar";
+import {MatTableDataSource} from "@angular/material/table";
+import {ProductService} from "../../services/product.service";
+import {MatDialog} from "@angular/material/dialog";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-product',
@@ -7,9 +14,125 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private productService: ProductService,
+              public dialog: MatDialog, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  displayedColumns: string[] = ['id', 'name', 'price', 'account', 'category', 'picture',  'actions'];
+  dataSource = new MatTableDataSource<ProductElement>();
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  getProducts(){
+    this.productService.getProducts()
+      .subscribe( (data:any) => {
+        console.log("respuesta de productos: ", data);
+        this.processProductResponse(data);
+      }, (error: any) => {
+        console.log("error en productos: ", error);
+      })
+  }
+
+  processProductResponse(resp: any){
+    const dateProduct: ProductElement[] = [];
+    if( resp.metadata[0].code == "00"){
+      let listCProduct = resp.product.products;
+
+      listCProduct.forEach((element: ProductElement) => {
+        //element.category = element.category.name;
+        element.picture = 'data:image/jpeg;base64,'+element.picture;
+        dateProduct.push(element);
+      });
+
+      //set the datasource
+      this.dataSource = new MatTableDataSource<ProductElement>(dateProduct);
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  openProductDialog(){
+    const dialogRef = this.dialog.open(NewProductComponent , {
+      width: '450px'
+    });
+
+    dialogRef.afterClosed().subscribe((result:any) => {
+
+      if( result == 1){
+        this.openSnackBar("Producto Agregado", "Exitosa");
+        this.getProducts();
+      } else if (result == 2) {
+        this.openSnackBar("Se produjo un error al guardar producto", "Error");
+      }
+    });
+  }
+
+  openSnackBar(message: string, action: string) : MatSnackBarRef<SimpleSnackBar>{
+    return this.snackBar.open(message, action, {
+      duration: 2000
+    })
+
+  }
+
+  edit(id:number, name: string, price: number, account: number, category: any){
+    const dialogRef = this.dialog.open(NewProductComponent , {
+      width: '450px',
+      data: {id: id, name: name, price: price, account: account, category: category}
+    });
+
+    dialogRef.afterClosed().subscribe((result:any) => {
+
+      if( result == 1){
+        this.openSnackBar("Producto editado", "Exitosa");
+        this.getProducts();
+      } else if (result == 2) {
+        this.openSnackBar("Se produjo un error al editar producto", "Error");
+      }
+    });
+
+
+  }
+
+  delete(id: any){
+    const dialogRef = this.dialog.open(ConfirmComponent , {
+      width: '450px',
+      data: {id: id, module: "product"}
+    });
+
+    dialogRef.afterClosed().subscribe((result:any) => {
+
+      if( result == 1){
+        this.openSnackBar("Producto eliminado", "Exitosa");
+        this.getProducts();
+      } else if (result == 2) {
+        this.openSnackBar("Se produjo un error al eliminar producto", "Error");
+      }
+    });
+  }
+
+  buscar(name: any){
+    if ( name.length === 0){
+      return this.getProducts();
+    }
+
+    this.productService.getProductByName(name)
+      .subscribe( (resp: any) =>{
+        this.processProductResponse(resp);
+      })
   }
 
 }
+
+export interface ProductElement {
+  id: number;
+  name: string;
+  price: number;
+  account: number;
+  category: any;
+  picture: any;
+}
+
